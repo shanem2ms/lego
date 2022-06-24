356cde6d3f02c4bc0282e4acf43f1759ccb875ac
Shane Morrison <shanemor@gmail.com>
2022-06-24 14:30:21 -0400

    from script

---
+++ setupsvr.sh
@@ -0,0 +1,15 @@
+apt update
+apt install cmake build-essential
+apt-get install pkg-config
+apt-get install curl zip unzip tar
+
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg/
+./bootstrap-vcpkg.sh 
+./vcpkg install zlib fmt enet libzip curl mongo-cxx-driver cxxopts nlohmann-json
+cd ~
+mkdir lego
+cd lego/
+mkdir bld
+ln -s /mnt/c/homep4/lego lego
+cmake -DVCPKG_DIR=/home/shanem/vcpkg -DVCPKG_INSTALL_PATH=/home/shanem/vcpkg/installed/x64-linux ../lego
