a938524dcddb3ca43457e2eab9a979c920c59b90
Shane Morrison <shanemor@gmail.com>
2022-09-09 22:04:43 -0400

    from script

---
+++ partmake/Scripts/Terrain/VtxBlit.glsl
@@ -0,0 +1,15 @@
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+#extension GL_ARB_shading_language_420pack : enable
+
+layout(location = 0) in vec3 Position;
+layout(location = 1) in vec3 Normal;
+layout(location = 2) in vec2 Texcoord;
+
+layout(location = 0) out vec2 fsin_0;
+
+void main()
+{
+    fsin_0 = vec2(Position.xy);
+    gl_Position = vec4(Position, 1);
+}
---
+++ partmake/Scripts/VoxelVis/VoxelVis.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.IO;
+using Veldrid;
+using Veldrid.SPIRV;
+
+namespace partmake.script
+{
+    public class VoxelVis
+    {
+	    private Texture _terrainTexture;
+	    private TextureView _terrainTextureView;
+	    private Sampler _terrainSampler;
+	    private DeviceBuffer _cubeVertexBuffer;
+	    private DeviceBuffer _cubeIndexBuffer;
+        private DeviceBuffer _projectionBuffer;
+        private DeviceBuffer _viewBuffer;
+        private DeviceBuffer _worldBuffer;
+        private DeviceBuffer _materialBuffer;
+        private Pipeline _pipeline;
+        private ResourceSet _projViewSet;
+        private ResourceSet _worldTextureSet;
+
+	    private uint _cubeIndexCount = 0;
+	    
+    	public void Gen(Texture terrainTexture, TextureView terrainTextureView)    
+    	{
+            _terrainTexture = terrainTexture;
+            _terrainTextureView = terrainTextureView;
+
+            _terrainSampler = Api.ResourceFactory.CreateSampler(new SamplerDescription(SamplerAddressMode.Clamp, SamplerAddressMode.Clamp, SamplerAddressMode.Clamp, SamplerFilter.MinPoint_MagPoint_MipPoint,
+                ComparisonKind.Always, 0, 0, 0, 0, SamplerBorderColor.OpaqueBlack));
+
+			byte []pixShaderBytes = File.ReadAllBytes(Path.Combine(Api.ScriptFolder, "VoxelVis\\VoxVisFrag.glsl"));    		
+    		byte []vtxShaderBytes = File.ReadAllBytes(Path.Combine(Api.ScriptFolder, "VoxelVis\\VoxVisVtx.glsl"));    		
+    		
+            ShaderSetDescription shaderSet = new ShaderSetDescription(
+                new[]
+                {
+                new VertexLayoutDescription(
+                    new VertexElementDescription("Position", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float3),
+                    new VertexElementDescription("Normal", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float3),
+                    new VertexElementDescription("TexCoords", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float2))
+                },
+                Api.ResourceFactory.CreateFromSpirv(
+	                new ShaderDescription(ShaderStages.Vertex, vtxShaderBytes, "main"),
+	                new ShaderDescription(ShaderStages.Fragment, pixShaderBytes, "main")));
+    		
+            var cubeVertices = primitives.Cube.GetVertices();
+            _cubeVertexBuffer = Api.ResourceFactory.CreateBuffer(new BufferDescription((uint)(Vtx.SizeInBytes * cubeVertices.Length), BufferUsage.VertexBuffer));
+            Api.GraphicsDevice.UpdateBuffer(_cubeVertexBuffer, 0, cubeVertices);
+
+            var cubeIndices = primitives.Cube.GetIndices();
+            _cubeIndexBuffer = Api.ResourceFactory.CreateBuffer(new BufferDescription(sizeof(ushort) * (uint)cubeIndices.Length, BufferUsage.IndexBuffer));
+            _cubeIndexCount = (uint)cubeIndices.Length;
+            Api.GraphicsDevice.UpdateBuffer(_cubeIndexBuffer, 0, cubeIndices);
+
+			_projectionBuffer = Api.ResourceFactory.CreateBuffer(new BufferDescription(64, BufferUsage.UniformBuffer));
+            _viewBuffer = Api.ResourceFactory.CreateBuffer(new BufferDescription(64, BufferUsage.UniformBuffer));
+            _worldBuffer = Api.ResourceFactory.CreateBuffer(new BufferDescription(64, BufferUsage.UniformBuffer));
+            _materialBuffer = Api.ResourceFactory.CreateBuffer(new BufferDescription(16, BufferUsage.UniformBuffer));
+
+            
+			ResourceLayout projViewLayout = Api.ResourceFactory.CreateResourceLayout(
+                new ResourceLayoutDescription(
+                    new ResourceLayoutElementDescription("ProjectionBuffer", ResourceKind.UniformBuffer, ShaderStages.Vertex),
+                    new ResourceLayoutElementDescription("ViewBuffer", ResourceKind.UniformBuffer, ShaderStages.Vertex)));
+
+            ResourceLayout worldTextureLayout = Api.ResourceFactory.CreateResourceLayout(
+                new ResourceLayoutDescription(
+                    new ResourceLayoutElementDescription("WorldBuffer", ResourceKind.UniformBuffer, ShaderStages.Vertex),
+                    new ResourceLayoutElementDescription("MeshColor", ResourceKind.UniformBuffer, ShaderStages.Fragment),
+                    new ResourceLayoutElementDescription("VoxTexture", ResourceKind.TextureReadOnly, ShaderStages.Fragment),
+                    new ResourceLayoutElementDescription("VoxSampler", ResourceKind.Sampler, ShaderStages.Fragment)));            
+                    
+            _projViewSet = Api.ResourceFactory.CreateResourceSet(new ResourceSetDescription(
+                projViewLayout,
+                _projectionBuffer,
+                _viewBuffer));
+
+            _worldTextureSet = Api.ResourceFactory.CreateResourceSet(new ResourceSetDescription(
+                worldTextureLayout,
+                _worldBuffer,
+                _materialBuffer,
+                _terrainTextureView,
+                _terrainSampler));
+                                
+            _pipeline = Api.ResourceFactory.CreateGraphicsPipeline(new GraphicsPipelineDescription(
+                BlendStateDescription.SingleAlphaBlend,
+                DepthStencilStateDescription.DepthOnlyLessEqual,
+                new RasterizerStateDescription(FaceCullMode.None, PolygonFillMode.Solid, FrontFace.CounterClockwise, false, false),
+                PrimitiveTopology.TriangleList,
+                shaderSet,
+                new[] { projViewLayout, worldTextureLayout },
+                Api.Swapchain.Framebuffer.OutputDescription));            
+    	}
+    	
+    	public void Draw(CommandList cl, ref Matrix4x4 viewmat, ref Matrix4x4 projMat)
+    	{
+    		if (_cubeIndexCount == 0)
+    			return;
+    	 	cl.SetPipeline(_pipeline);
+            cl.SetFramebuffer(Api.Swapchain.Framebuffer);
+            cl.SetGraphicsResourceSet(0, _projViewSet);
+            cl.SetGraphicsResourceSet(1, _worldTextureSet);
+    	 	
+            Matrix4x4 mat =
+                Matrix4x4.CreateScale(0.1f) *
+                Matrix4x4.CreateTranslation(new Vector3(0, 0, 0));
+
+            cl.UpdateBuffer(_projectionBuffer, 0, ref projMat);
+            cl.UpdateBuffer(_viewBuffer, 0, viewmat);
+            cl.UpdateBuffer(_worldBuffer, 0, ref mat);
+            Vector4 color = new Vector4(1, 0.5f, 0.5f, 1);
+            cl.UpdateBuffer(_materialBuffer, 0, ref color);
+            cl.SetVertexBuffer(0, _cubeVertexBuffer);
+            cl.SetIndexBuffer(_cubeIndexBuffer, IndexFormat.UInt16);
+            cl.DrawIndexed(_cubeIndexCount);            
+    	}
+    }
+}    
